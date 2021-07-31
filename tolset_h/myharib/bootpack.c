f6fa230c035d6c5fc53a2c377f4a9f5140264e5a
kadoyau <el.psi.congroo@gmail.com>
2021-07-31 19:28:22 +0900

    harib01c: ポインタを使って書き直す

--- tolset_h/myharib/bootpack.c
+++ tolset_h/myharib/bootpack.c
@@ -1,6 +1,5 @@
 /* 他のファイルで作った関数がありますとCコンパイラに教える */
 void io_hlt(void);
-void write_mem8(int addr, int data);
 
 /* 関数宣言なのに、{}がなくていきなり;を書くと、
 	他のファイルにあるからよろしくね、という意味になるのです。 */
@@ -8,10 +7,13 @@ void write_mem8(int addr, int data);
 void HariMain(void)
 {
 	int i; /* 32 bit整数型 */
+	char *p; /* pという変数は、BYTE [...]用の番地 */
+
 
 	for (i = 0xa0000; i <= 0xaffff; i++) {
+		p = (char *) i; /* 番地を代入 */
 	    /* 下位4bitのANDをとってしましま模様を出す */
-		write_mem8(i, i & 0x0f); /* MOV BYTE [i], i & 0x0f */
+		*p = i & 0x0f;
 	}
 
 	for (;;) {
--- tolset_h/myharib/naskfunc.nas
+++ tolset_h/myharib/naskfunc.nas
@@ -12,7 +12,7 @@
 
 [FILE "naskfunc.nas"]			; ソースファイル名情報。naskのオブジェクトファイルモードでは必須。
 
-		GLOBAL	_io_hlt,_write_mem8			; このプログラムに含まれる関数名。prefixの_は命名規則
+		GLOBAL	_io_hlt			; このプログラムに含まれる関数名。prefixの_は命名規則
 
 
 ; 以下は実際の関数
@@ -21,11 +21,4 @@
 
 _io_hlt:	; void io_hlt(void); HLT命令はI/O命令に属する
 		HLT
-		RET
-
-; C言語に直接メモリ番地を指定して書くための関数
-_write_mem8:	; void write_mem8(int addr, int data);
-		MOV		ECX,[ESP+4]		; [ESP+4]にaddrが入っているのでそれをECXに読み込む
-		MOV		AL,[ESP+8]		; [ESP+8]にdataが入っているのでそれをALに読み込む
-		MOV		[ECX],AL
 		RET
